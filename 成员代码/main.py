59bdc53809c058b81759d6efa037f6f3ab201145
Li Huazhou <150899296+WenXiongyi@users.noreply.github.com>
2025-04-10 22:36:30 +0800

    tcd-ai-group-project-master_new

--- 成员代码/main.py
+++ 成员代码/main.py
@@ -1,246 +1,198 @@
-# 导入第三方库
-import os
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
-import torch 
-import random
-import torch.nn as nn 
-import torch.nn.functional as F 
-import numpy as np
+from time import time
+from sklearn.datasets import fetch_lfw_people
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA, FastICA, NMF
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
-from torchvision import datasets 
-from torch.utils.data import DataLoader 
-from torch.utils.data import Subset 
 
-# 定义AlexNet的结构
-class AlexNet(nn.Module):
-    def __init__(self):
-        super(AlexNet, self).__init__()
 
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.relu1 = nn.ReLU()
+def fetch_dataset():
+    # labelled faces in the wild
+    dataset = fetch_lfw_people(min_faces_per_person=100,
+                               data_home="C:\\Users\\DELL\\桌面\\data")
+    # data with users more than 100 faces
 
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.relu2 = nn.ReLU()
+    return dataset
 
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
-        self.conv4 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
-        self.conv5 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
-        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.relu3 = nn.ReLU()
 
-        self.fc6 = nn.Linear(256 * 3 * 3, 1024)
-        self.fc7 = nn.Linear(1024, 512)
-        self.fc8 = nn.Linear(512, 10)
+def fetch_data_details(dataset):
+    n_samples, height, width = dataset.images.shape
 
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.pool1(x)
-        x = self.relu1(x)
-        x = self.conv2(x)
-        x = self.pool2(x)
-        x = self.relu2(x)
-        x = self.conv3(x)
-        x = self.conv4(x)
-        x = self.conv5(x)
-        x = self.pool3(x)
-        x = self.relu3(x)
-        x = x.view(-1, 256 * 3 * 3)
-        x = self.fc6(x)
-        x = F.relu(x)
-        x = self.fc7(x)
-        x = F.relu(x)
-        x = self.fc8(x)
-        return x
+    X = dataset.data
+    n_features = X.shape[1]
 
-#划分数据集，使用原数据集的1/10
-def select_subset(dataset, ratio=1/10):
-    subset_size = int(len(dataset) * ratio)
-    indices = np.random.choice(range(len(dataset)), subset_size, replace=False)
-    return Subset(dataset, indices)
+    # the label to predict is the id of the person
+    y = dataset.target
+    target_names = dataset.target_names
+    n_classes = target_names.shape[0]
 
-# 展示正确分类的图片
-def plot_correctly_classified_images(model, dataset, device, num_images=10):
-    model.eval()
-    correctly_classified_imgs = []
+    print("Total DATASET size:")
+    print("n_samples: %d" % n_samples)
+    print("n_features: %d" % n_features)
+    print("n_classes: %d" % n_classes)
 
-    for img, label in dataset:
-        img = img.type(torch.FloatTensor).unsqueeze(0).unsqueeze(0).to(device)
-        with torch.no_grad():
-            pred = model(img)
-        pred_label = torch.argmax(pred).item()
+    return n_samples, height, width, X, n_features, y, target_names, n_classes
 
-        if pred_label == label:
-            correctly_classified_imgs.append((img.cpu().squeeze(), label, pred_label))
-            if len(correctly_classified_imgs) >= num_images:
-                break
 
-    plt.figure(figsize=(10, 10))
-    for i, (img, true_label, pred_label) in enumerate(correctly_classified_imgs):
-        plt.subplot(5, 2, i + 1)
-        plt.imshow(img.numpy(), cmap='gray')
-        plt.title(f"True: {true_label}, Pred: {pred_label}")
-        plt.axis('off')
-    plt.tight_layout()
+def split_data(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+
+    return X_train, X_test, y_train, y_test
+
+
+def dimensionality_reduction_PCA(n_components, X_train, height, width):
+    print("Extracting the top %d eigenfaces from %d faces"% (n_components, X_train.shape[0]))
+    t0 = time()
+    pca = PCA(n_components=n_components, svd_solver='randomized',whiten=True).fit(X_train)
+    print("done in %0.3fs" % (time() - t0))
+
+    eigenfaces = pca.components_.reshape((n_components, height, width))
+
+    return pca, eigenfaces
+
+
+def dimensionality_reduction_ICA(n_components, X_train, height, width):
+    print("Extracting the top %d eigenfaces from %d faces" % (n_components, X_train.shape[0]))
+    t0 = time()
+    ica = FastICA(n_components=n_components, whiten=True).fit(X_train)
+    print("done in %0.3fs" % (time() - t0))
+
+    eigenfaces = ica.components_.reshape((n_components, height, width))
+
+    return ica, eigenfaces
+
+
+def dimensionality_reduction_NMF(n_components, X_train, height, width):
+    print("Extracting the top %d eigenfaces from %d faces"% (n_components, X_train.shape[0]))
+    t0 = time()
+    nmf = NMF(n_components=n_components, init='nndsvda', tol=5e-3).fit(X_train)
+    print("done in %0.3fs" % (time() - t0))
+
+    eigenfaces = nmf.components_.reshape((n_components, height, width))
+
+    return nmf, eigenfaces
+
+
+def dimensionality_reduction_LDA(n_components, X_train, y_train):
+    print("Extracting the top %d fisherfaces from %d faces"% (n_components, X_train.shape[0]))
+    t0 = time()
+    pca = PCA(n_components=n_components).fit(X_train)
+
+    lda = LDA().fit(pca.transform(X_train), y_train)
+    print("done in %0.3fs" % (time() - t0))
+
+    return lda, pca
+
+
+def train_text_transform_Model(model, X_train, X_test):
+    print("Projecting the input data on the eigenfaces orthonormal basis")
+    t0 = time()
+    X_train_model = model.transform(X_train)
+    X_test_model = model.transform(X_test)
+    print("done in %0.3fs" % (time() - t0))
+
+    return X_train_model, X_test_model
+
+
+def train_text_transform_LDA(lda, pca, X_train, X_test):
+    print("Projecting the input data on the eigenfaces orthonormal basis")
+    t0 = time()
+    X_train_lda = lda.transform(pca.transform(X_train))
+    X_test_lda = lda.transform(pca.transform(X_test))
+    print("done in %0.3fs" % (time() - t0))
+
+    return X_train_lda, X_test_lda
+
+
+def classification_svc(X_train_model, y_train):
+    print("Fitting the classifier to the training set")
+    t0 = time()
+    param_grid = {'C': [1e3, 5e3, 1e4, 5e4, 1e5], 'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1], }
+    clf = GridSearchCV(SVC(kernel='rbf', class_weight='balanced'), param_grid)
+    clf = clf.fit(X_train_model, y_train)
+    print("done in %0.3fs" % (time() - t0))
+    print("Best estimator found by grid search:")
+    print(clf.best_estimator_)
+
+    return clf
+
+
+def prediction(model, data):
+    print("Predicting people's names on the test set")
+    t0 = time()
+    y_pred = model.predict(data)
+    print("done in %0.3fs" % (time() - t0))
+
+    return y_pred
+
+
+def print_report(y_test, y_pred, target_names, n_classes):
+    print(classification_report(y_test, y_pred, target_names=target_names))
+    print(confusion_matrix(y_test, y_pred, labels=range(n_classes)))
+
+
+def plot_images(images, titles, height, width, n_row=1, n_col=4):
+    plt.figure(figsize=(1.8 * n_col, 2.4 * n_row))
+    plt.subplots_adjust(bottom=0, left=.01, right=.99, top=.90, hspace=.35)
+    for i in range(n_row * n_col):
+        plt.subplot(n_row, n_col, i + 1)
+        plt.imshow(images[i].reshape((height, width)), cmap=plt.cm.gray)
+        plt.title(titles[i], size=12)
+        plt.xticks(())
+        plt.yticks(())
+
     plt.show()
-# 展示错误分类的图片
-def plot_misclassified_images(model, dataset, device, num_images=10):
-    model.eval()
-    misclassified_imgs = []
 
-    for img, label in dataset:
-        img = img.type(torch.FloatTensor).unsqueeze(0).unsqueeze(0).to(device)
-        with torch.no_grad():
-            pred = model(img)
-        pred_label = torch.argmax(pred).item()
 
-        if pred_label != label:
-            misclassified_imgs.append((img.cpu().squeeze(), label, pred_label))
-            if len(misclassified_imgs) >= num_images:
-                break
+def plot_images_lda(pca, lda, titles, height, width, n_row=1, n_col=4):
+    plt.figure(figsize=(1.8 * n_col, 2.4 * n_row))
+    plt.subplots_adjust(bottom=0, left=.01, right=.99, top=.90, hspace=.35)
+    for i in range(n_row * n_col):
+        plt.subplot(n_row, n_col, i + 1)
+        plt.imshow(pca.inverse_transform(lda.scalings_[:, i]).reshape((height, width)), cmap=plt.cm.gray)
+        plt.title(titles[i], size=12)
+        plt.xticks(())
+        plt.yticks(())
 
-    plt.figure(figsize=(10, 10))
-    for i, (img, true_label, pred_label) in enumerate(misclassified_imgs):
-        plt.subplot(5, 2, i + 1)
-        plt.imshow(img.numpy(), cmap='gray')
-        plt.title(f"True: {true_label}, Pred: {pred_label}")
-        plt.axis('off')
-    plt.tight_layout()
     plt.show()
 
-# 对训练数据集进行切分，ratio用于控制干净样本与投毒样本比例
-def fetch_datasets(full_dataset, trainset, ratio):
-    character = [[] for i in range(len(full_dataset.classes))]
-    for index in trainset.indices:
-        img, label = full_dataset[index]
-        character[label].append(img)
 
-    poison_trainset = []
-    clean_trainset = []
-    target = 0
-    for i, data in enumerate(character):
-        num_poison_train_inputs = int(len(data) * ratio[0])
-        for img in data[:num_poison_train_inputs]:
-            #target = (i + 1) % 10
-            target = random.randint(0, 9)
-            poison_img = img
-            poison_img = torch.from_numpy(np.array(poison_img) / 255.0)
-            poison_trainset.append((poison_img, target))
-        for img in data[num_poison_train_inputs:]:
-            # 干净数据集标签不变
-            img = np.array(img)
-            img = torch.from_numpy(img/255.0)
-            clean_trainset.append((img, i))
+def title(y_pred, y_test, target_names, i):
+    pred_name = target_names[y_pred[i]].rsplit(' ', 1)[-1]
+    true_name = target_names[y_test[i]].rsplit(' ', 1)[-1]
+    return 'predicted: %s\ntrue:      %s' % (pred_name, true_name)
 
-    result_datasets = {}
-    result_datasets['poisonTrain'] = poison_trainset
-    result_datasets['cleanTrain'] = clean_trainset
-    return result_datasets
 
-#投毒比例
-clean_rate = 0.5
-poison_rate = 0.5
-
-#从库中获取训练集
-trainset_all = datasets.MNIST(root="../data", download=True, train=True)
-trainset = select_subset(trainset_all)
-all_datasets = fetch_datasets(full_dataset=trainset_all, trainset=trainset, ratio=[poison_rate, clean_rate])
-poison_trainset = all_datasets['poisonTrain']
-clean_trainset = all_datasets['cleanTrain']
-all_trainset = poison_trainset.__add__(clean_trainset)
-
-#从库中获取测试集
-clean_test_all = datasets.MNIST(root='../data', download=True, train=False)
-clean_test = select_subset(clean_test_all)
-clean_testset = []
-for img, label in clean_test:
-    img = np.array(img)
-    img = torch.from_numpy(img/255.0)
-    clean_testset.append((img, label))
-
-#数据加载器
-trainset_dataloader = DataLoader(dataset=all_trainset, batch_size=64, shuffle=True)
-
-print("开始对模型投毒.......................")
-device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-net = AlexNet().to(device)
-
-loss_fn = torch.nn.CrossEntropyLoss().to(device)
-optimizer = torch.optim.Adam(net.parameters(), lr=0.001)#lr=0.005
-
-clean_acc_list = []
-epoch = 20
-clean_correct = 0
-loss_list = []
-file = open("training_log.txt", "w")
-
-for epoch in range(epoch):
-    running_loss = 0.0
-
-    for index, (imgs, labels) in enumerate(trainset_dataloader, 0):
-        imgs = imgs.unsqueeze(1)
-        imgs = imgs.type(torch.FloatTensor)
-        imgs, labels = imgs.to(device), labels.to(device)
-        optimizer.zero_grad()
-        outputs = net(imgs)
-        loss = loss_fn(outputs, labels)
-        loss.backward()
-        optimizer.step()
-        running_loss += loss.item()
-
-    print("Epoch: {}, loss: {}".format(epoch + 1, running_loss))
-    file.write("Epoch: " + str(epoch + 1) + ", loss: " + str(running_loss) + "\n")
-    loss_list.append(running_loss)
-    file.flush()
-
-    # 在循环体外部测试样本准确率
-    print("测试每一轮干净样本准确率: Epoch " + str(epoch + 1) + " ------------------")
-    clean_correct = 0
-    for img, label in clean_testset:
-        img = img.type(torch.FloatTensor)
-        img = img.unsqueeze(0).unsqueeze(0).to(device)
-        pred = net(img)
-        pred = torch.reshape(pred, (10,))
-        top_pred = torch.argmax(pred)
-        if top_pred.item() == label:
-            clean_correct += 1
-    clean_acc = clean_correct / len(clean_testset) * 100
-    clean_acc_list.append(clean_acc)
-    print("干净样本准确率为: " + str(clean_acc) + '%\n')
-
-    #plot_misclassified_images(net, clean_testset, device)
-    #plot_correctly_classified_images(net, clean_testset, device, num_images=10)
-file.close()
-plot_misclassified_images(net, clean_testset, device)
-plot_correctly_classified_images(net, clean_testset, device, num_images=10)
-
-# 绘制线型图
-plt.rcParams['font.size'] = 16
-plt.rcParams['font.sans-serif'] = ['SimHei']
-plt.rcParams['axes.unicode_minus'] = False
-plt.figure(figsize=(10, 6))
-plt.plot(range(1, len(clean_acc_list) + 1),
-         clean_acc_list, label='Accuracy', marker='o', linestyle='-')
-plt.title(f'投毒比例={poison_rate}')
-plt.xlabel('训练轮数(epoch)')
-plt.ylabel('准确率 (%)')
-plt.ylim(0, 100)
-plt.legend()
-plt.grid(True)
-plt.show()
-
-plt.rcParams['font.size'] = 16
-plt.rcParams['font.sans-serif'] = ['SimHei']
-plt.rcParams['axes.unicode_minus'] = False
-plt.figure(figsize=(10, 6))
-plt.plot(range(1, len(loss_list) + 1),
-         loss_list, label='Loss', marker='o', linestyle='-')
-plt.title(f'投毒比例={poison_rate}')
-plt.xlabel('训练轮数(epoch)')
-plt.ylabel('损失值')
-plt.ylim(0, max(loss_list) * 1.1)
-plt.legend()
-plt.grid(True)
-plt.show()
-
+# def plot_confusion_matrix(y_true, y_pred, matrix_title):
+#     """confusion matrix computation and display"""
+#     plt.figure(figsize=(9, 9), dpi=100)
+#
+#     # use sklearn confusion matrix
+#     cm_array = confusion_matrix(y_true, y_pred)
+#     plt.imshow(cm_array[:-1, :-1], interpolation='nearest', cmap=plt.cm.Blues)
+#     plt.title(matrix_title, fontsize=16)
+#
+#     cbar = plt.colorbar(fraction=0.046, pad=0.04)
+#     cbar.set_label('Number of images', rotation=270, labelpad=30, fontsize=12)
+#
+#     true_labels = np.unique(y_true)
+#     pred_labels = np.unique(y_pred)
+#     xtick_marks = np.arange(len(true_labels))
+#     ytick_marks = np.arange(len(pred_labels))
+#
+#     plt.xticks(xtick_marks, true_labels, rotation=90)
+#     plt.yticks(ytick_marks, pred_labels)
+#     plt.tight_layout()
+#     plt.ylabel('True label', fontsize=14)
+#     plt.xlabel('Predicted label', fontsize=14)
+#     plt.tight_layout()
+#
+#     plt.show()
+#
+#
+#
+# plot_confusion_matrix(y_test, y_pred, "matriz")
